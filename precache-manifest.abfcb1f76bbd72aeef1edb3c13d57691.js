15b1494f837daa4844a287afdf427df0080b56a7
angelico <angelico@betalearning.com.br>
2021-04-14 17:43:42 -0300

    deploy

---
+++ precache-manifest.abfcb1f76bbd72aeef1edb3c13d57691.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6815d8f65d10ade7dc3",
+    "url": "/css/app.14d96648.css"
+  },
+  {
+    "revision": "dcf6abc8f5592738388c",
+    "url": "/css/chunk-vendors.af3221a8.css"
+  },
+  {
+    "revision": "61fb69c8836212dd7f10f86c764445ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6815d8f65d10ade7dc3",
+    "url": "/js/app.50928d9a.js"
+  },
+  {
+    "revision": "dcf6abc8f5592738388c",
+    "url": "/js/chunk-vendors.fa2c60a6.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
